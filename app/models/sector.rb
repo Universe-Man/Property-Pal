cf08cd4386f2d60d6d49e628807ced79dea8eb10
github username <ianjpollack@gmail.com>
2018-05-08 19:41:19 -0400

    sector edit form fix, model validations fixed

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -3,5 +3,5 @@ class Sector < ApplicationRecord
   has_many :units
   has_many :tenants, through: :units
   validates :name, presence: true
-  # validates :unit_id, presence: true
+  validates :property_id, presence: true
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -4,9 +4,8 @@ class Tenant < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
-  # validates :address_id, presence: true
-  # validates :contract_id, presence: true
+  validates :password_digest, presence: true
+  validates :contract_id, presence: true
 
   def fullname
     self.first_name + " " + self.last_name
