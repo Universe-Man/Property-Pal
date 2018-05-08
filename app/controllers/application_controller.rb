b6f17c1ac9895edabb969d4ef428e5d6f24cead9
rogercodes1 <roger.ucsb@gmail.com>
2018-05-08 15:03:43 -0400

    fixed shit. added bcrypt info.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,19 @@
 class ApplicationController < ActionController::Base
-  helper_method :logged_in?
 
-    def current_user
-      if session[:user_id]
-        User.find(session[:user_id])
-      end
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
     end
+  end
 
-    def logged_in?
-      !!current_user
-      # !<user instance> -> false (negate that to get true)
-      # !nil -> true (negate that to get false)
-    end
+  def logged_in?
+    !!current_user #double negation, if true user exists, false if it doesn't
+    #!<user instance> --> falase
+    # !nil --> true (negate taht to a false) 
+  end
 
-    def authorized
-      redirect_to login_path unless logged_in?
-    end
 
-    def authorized_for_user(user)
-      authorized
-      if current_user != user
-        redirect_to current_user
-      end
-    end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,17 @@ Rails.application.routes.draw do
   resources :properties
   resources :admins
   resources :tenants
-  # get "/login", to: 'static#login'
+
+  get "/login", to: 'static#login'
   get "/admin_login", to: 'admins#admin_login'
   get '/properties/:id/sectors', to: 'sector#show'
   get '/sectors/:id/units', to: 'unit#show'
 
 
+  get "/tenant_login", to: 'tenants#tenant_login'
 
+  get "tenant_homepage", to: 'tenant#home'
+  # post "/sessions", to: 'sessions#create'
   # post "/logout", to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
